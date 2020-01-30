9843b73be80a0954af2f94e39718ff43c202fa01
dw1 <dwi.siswanto98@gmail.com>
2020-01-30 16:36:39 +0700

    add condition event only in PR

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
+	EVENT_ACTION=$(jq -r ".action" "${GITHUB_EVENT_PATH}")
+	if [[ "${EVENT_ACTION}" != "opened" ]]; then
+		echo "No need to run analysis. It is already triggered by the push event."
+		exit 78
+	fi
+fi
+
 if [[ -z "${INPUT_PASSWORD}" ]]; then
 	SONAR_PASSWORD="&& true"
 else
