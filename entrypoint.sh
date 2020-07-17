545a1a5fac428edfe0fd06bf78d283007663b00c
Dwi Siswanto <dwi.siswanto98@gmail.com>
2020-07-17 09:30:09 +0700

    :heavy_minus_sign: Remove unused variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -25,6 +25,4 @@ sonar-scanner \
 	-Dsonar.login=${INPUT_LOGIN} \
 	-Dsonar.password=${INPUT_PASSWORD} \
 	-Dsonar.sources=. \
-	-Dsonar.sourceEncoding=UTF-8 \
-	${SONAR_PASSWORD}
-
+	-Dsonar.sourceEncoding=UTF-8
