9b7b1d0336263a028b417c3ea63e1c221f636bbf
Alexander Jank <alex@zeraton.de>
2020-12-17 23:23:49 +0000

    update base image; respect sonar-project.properties

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,17 +13,26 @@ fi
 REPOSITORY_NAME=$(basename "${GITHUB_REPOSITORY}")
 
 [[ ! -z ${INPUT_PASSWORD} ]] && SONAR_PASSWORD="${INPUT_PASSWORD}" || SONAR_PASSWORD=""
-[[ -z ${INPUT_PROJECTKEY} ]] && SONAR_PROJECTKEY="${REPOSITORY_NAME}" || SONAR_PROJECTKEY="${INPUT_PROJECTKEY}"
-[[ -z ${INPUT_PROJECTNAME} ]] && SONAR_PROJECTNAME="${REPOSITORY_NAME}" || SONAR_PROJECTNAME="${INPUT_PROJECTNAME}"
-[[ -z ${INPUT_PROJECTVERSION} ]] && SONAR_PROJECTVERSION="" || SONAR_PROJECTVERSION="${INPUT_PROJECTVERSION}"
 
-sonar-scanner \
-	-Dsonar.host.url=${INPUT_HOST} \
-	-Dsonar.projectKey=${SONAR_PROJECTKEY} \
-	-Dsonar.projectName=${SONAR_PROJECTNAME} \
-	-Dsonar.projectVersion=${SONAR_PROJECTVERSION} \
-	-Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
-	-Dsonar.login=${INPUT_LOGIN} \
-	-Dsonar.password=${INPUT_PASSWORD} \
-	-Dsonar.sources=. \
-	-Dsonar.sourceEncoding=UTF-8
+if [! -f "./sonar-project.properties"]
+then
+  [[ -z ${INPUT_PROJECTKEY} ]] && SONAR_PROJECTKEY="${REPOSITORY_NAME}" || SONAR_PROJECTKEY="${INPUT_PROJECTKEY}"
+  [[ -z ${INPUT_PROJECTNAME} ]] && SONAR_PROJECTNAME="${REPOSITORY_NAME}" || SONAR_PROJECTNAME="${INPUT_PROJECTNAME}"
+  [[ -z ${INPUT_PROJECTVERSION} ]] && SONAR_PROJECTVERSION="" || SONAR_PROJECTVERSION="${INPUT_PROJECTVERSION}"
+  sonar-scanner \
+    -Dsonar.host.url=${INPUT_HOST} \
+    -Dsonar.projectKey=${SONAR_PROJECTKEY} \
+    -Dsonar.projectName=${SONAR_PROJECTNAME} \
+    -Dsonar.projectVersion=${SONAR_PROJECTVERSION} \
+    -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+    -Dsonar.login=${INPUT_LOGIN} \
+    -Dsonar.password=${SONAR_PASSWORD} \
+    -Dsonar.sources=. \
+    -Dsonar.sourceEncoding=UTF-8
+else
+  sonar-scanner \
+    -Dsonar.host.url=${INPUT_HOST} \
+    -Dsonar.projectBaseDir=${INPUT_PROJECTBASEDIR} \
+    -Dsonar.login=${INPUT_LOGIN} \
+    -Dsonar.password=${SONAR_PASSWORD}
+fi
